d1d5031ead40ec433aa61cfd60096c711de38a8b
Adrian Kurt <dilanmarinas@gmail.com>
2025-03-12 16:02:24 +0800

    rename and fix

--- fixed_batch_2/except_num_ends-5-0.py
+++ fixed_batch_2/except_num_ends-5-0.py
@@ -1,9 +1,8 @@
 # Prog09: Create a program that print all the numbers starting from 0 to 100 except numbers ending in zero or ending five.
 
-nums = []
-
+num_list = []
 for i in range(101):
     if i%5 != 0:
-        nums.append(i)
+        num_list.append(i)
 
-print(nums)
+print(num_list)
