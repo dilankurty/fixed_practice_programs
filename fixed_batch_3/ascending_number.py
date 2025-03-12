b1eace1d1bda955d6b098c2bd2f39ad6d17734db
Adrian Kurt <dilanmarinas@gmail.com>
2025-03-13 00:32:23 +0800

    input and try except added

--- fixed_batch_3/ascending_number.py
+++ fixed_batch_3/ascending_number.py
@@ -1,2 +1,8 @@
 # Prog05: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the number from lowest to highest. Clue: sort() function
 
+num_list = []
+while True:
+    try:
+        num = float(input("Enter a number: "))
+    except ValueError:
+        break
