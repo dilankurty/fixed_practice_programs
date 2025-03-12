6d2d8ab8ce3017eb0721ed8c2b5637f9c8022f69
Adrian Kurt <dilanmarinas@gmail.com>
2025-03-13 00:43:00 +0800

    added for loop for 10 input numbers

--- fixed_batch_4/display_duplicate_numbers.py
+++ fixed_batch_4/display_duplicate_numbers.py
@@ -1,2 +1,7 @@
 # Prog01: Create a program that ask user to input 10 numbers. Display all numbers that have duplicate.
 
+num_list = []
+
+for i in range(10):
+    num = float(input("Enter a number: "))
+    num_list.append(num)
