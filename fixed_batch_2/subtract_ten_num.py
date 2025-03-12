bc47b04684527f5525acbefa8d586c439158d4f8
Adrian Kurt <dilanmarinas@gmail.com>
2025-03-12 15:41:45 +0800

    rename and fix

--- fixed_batch_2/subtract_ten_num.py
+++ fixed_batch_2/subtract_ten_num.py
@@ -1,10 +1,10 @@
 # Prog06: Create a program that ask user to input 10 numbers. Print the result of the first number minus all of the remaining numbers.
 
-nums = []
+num_list = []
 
 for i in range(10):
     num = float(input(f"Enter number {i+1}: "))
-    nums.append(num)
+    num_list.append(num)
 
-difference = nums[0] - sum(nums[1:])
+difference = num_list[0] - sum(num_list[1:])
 print(difference)
