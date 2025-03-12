ad922d9b188624a36f9046e8c6bda33fcfa18c14
Adrian Kurt <dilanmarinas@gmail.com>
2025-03-12 23:40:21 +0800

    input 10 numbers and rename

--- fixed_batch_3/numbers_w-o_duplicate.py
+++ fixed_batch_3/numbers_w-o_duplicate.py
@@ -1,7 +1,6 @@
 # Prog01: Create a program that ask user to input 10 numbers. Display all numbers that don't have duplicates.
 
 num_list = []
-
 for i in range(10):
     num = float(input(f"Enter number {i + 1}: "))
     num_list.append(num)
