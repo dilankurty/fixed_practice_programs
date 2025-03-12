c99a7107d9f611ee6fd99fd8584718fd24ce8d7d
Adrian Kurt <dilanmarinas@gmail.com>
2025-03-13 01:31:35 +0800

    add while loop to input numbers

--- fixed_batch_4/number_w-most_num_of_dup.py
+++ fixed_batch_4/number_w-most_num_of_dup.py
@@ -1,2 +1,6 @@
 # Prog02: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the number with the most number of duplicate.
 
+num_list = []
+while True:
+    num = float(input("Enter a number: "))
+    num_list.append(num)
