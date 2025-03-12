cb1efe03106d644d293f5ad67e72748f00024514
Adrian Kurt <dilanmarinas@gmail.com>
2025-03-12 16:23:12 +0800

    input number

---
+++ fixed_batch_3/no_duplicate_num.py
@@ -0,0 +1,5 @@
+# Prog01: Create a program that ask user to input 10 numbers. Display all numbers that don't have duplicate.
+
+num_list = []
+for i in range(10):
+    num = float(input(f"Enter number {i + 1}: "))
