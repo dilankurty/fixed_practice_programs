0584df94eaf1a76508999bf6fc554115ec45d1c3
Adrian Kurt <dilanmarinas@gmail.com>
2025-03-13 00:45:27 +0800

    added another for loop to check dupes

--- fixed_batch_4/display_duplicate_numbers.py
+++ fixed_batch_4/display_duplicate_numbers.py
@@ -1,7 +1,12 @@
 # Prog01: Create a program that ask user to input 10 numbers. Display all numbers that have duplicate.
 
 num_list = []
-
 for i in range(10):
     num = float(input("Enter a number: "))
     num_list.append(num)
+
+duplicates = []
+for num in num_list:
+    if num_list.count(num) > 1:
+        duplicates.append(num)
+        print(duplicates)
