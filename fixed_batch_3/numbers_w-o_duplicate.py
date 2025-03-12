dfd49fcf06883eea6001905b0851450ff202f078
Adrian Kurt <dilanmarinas@gmail.com>
2025-03-12 23:48:16 +0800

    added a list of numbers without duplicates

--- fixed_batch_3/numbers_w-o_duplicate.py
+++ fixed_batch_3/numbers_w-o_duplicate.py
@@ -1,10 +1,13 @@
 # Prog01: Create a program that ask user to input 10 numbers. Display all numbers that don't have duplicates.
 
 num_list = []
+num_wo_duplicates = []
 for i in range(10):
     num = float(input(f"Enter number {i + 1}: "))
     num_list.append(num)
 
 for num in num_list:
     if num_list.count(num) == 1:
-            print(num)
+        num_wo_duplicates.append(num)
+
+print(num_wo_duplicates)
