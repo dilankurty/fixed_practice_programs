dc8c3878b79aa21ba4f4ab76dc58c8e058be4234
Adrian Kurt <dilanmarinas@gmail.com>
2025-03-12 14:48:31 +0800

    rename and fix

--- fixed_batch_1/prog09_batch1.py
+++
@@ -1,7 +0,0 @@
-# Prog09: Create a program that print all the even numbers starting from 0 to 100. (Use for loop)
-num = []
-for i in range(101):
-    if i%2 == 0:
-        num.append(i)
-
-print(num)
