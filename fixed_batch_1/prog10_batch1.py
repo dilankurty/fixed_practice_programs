11f15427bc864cefdd757ce07ac320c07d7346be
Adrian Kurt <dilanmarinas@gmail.com>
2025-03-12 14:51:02 +0800

    rename and fix

--- fixed_batch_1/prog10_batch1.py
+++
@@ -1,7 +0,0 @@
-# Prog10: Create a program that print all the numbers starting from 0 to 100 except numbers ending in zero.
-nums = []
-for i in range(101):
-    if i%10 != 0:
-        nums.append(i)
-
-print(nums)
