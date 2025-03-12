94867fed089bd0da23d8965bef6dc6e03261962b
Adrian Kurt <dilanmarinas@gmail.com>
2025-03-13 01:06:55 +0800

    added while loop for input

--- fixed_batch_4/descending_numbers.py
+++ fixed_batch_4/descending_numbers.py
@@ -1,2 +1,6 @@
 # Prog04: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the number from highest to lowest. Clue: sort() function
 
+num_list = []
+while True:
+    num = float(input("Enter a number: "))
+    num_list.append(num)
