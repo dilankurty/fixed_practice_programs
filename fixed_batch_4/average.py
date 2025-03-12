ac3045c2456ee835499065d224df33a348449730
Adrian Kurt <dilanmarinas@gmail.com>
2025-03-13 01:19:46 +0800

    added while for inputs

--- fixed_batch_4/average.py
+++ fixed_batch_4/average.py
@@ -1,2 +1,5 @@
 # Prog05: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the average.
 
+num_list = []
+while True:
+    num = float(input("Enter a number: "))
