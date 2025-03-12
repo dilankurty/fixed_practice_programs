56b7da96ee75bca37ace7c01603f5a73664812cc
Adrian Kurt <dilanmarinas@gmail.com>
2025-03-13 00:54:22 +0800

    added while loop and input

--- fixed_batch_4/highest_number.py
+++ fixed_batch_4/highest_number.py
@@ -1,1 +1,6 @@
 # Prog03: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the highest number.
+
+num_list = []
+while True:
+    num = input("Enter a number: ")
+    num_list.append(num)
