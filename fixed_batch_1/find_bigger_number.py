5c85cc841c68cfe8162b6d2e1d24d19464adfac4
Adrian Kurt <dilanmarinas@gmail.com>
2025-03-12 14:11:19 +0800

    rename and fix

--- fixed_batch_1/find_bigger_number.py
+++ fixed_batch_1/find_bigger_number.py
@@ -4,10 +4,8 @@ num1 = float(input("Enter the first number: "))
 num2 = float(input("Enter the second number: "))
 
 if num1 > num2:
-    print(f"The bigger number is {num1}")
-
+    print(f"Bigger number: {num1}")
 elif num2 > num1:
-    print(f"The bigger number is {num2}")
-
+    print(f"Bigger number: {num2}")
 else:
-    print("Both numbers are EQUAL")
+    print("Equal")
