0ab2f41d52e422448ffe4497be4ed328e5cca071
Adrian Kurt <dilanmarinas@gmail.com>
2025-03-13 00:20:15 +0800

    added input & while loop for prompting infinitely

--- fixed_batch_3/lowest_number.py
+++ fixed_batch_3/lowest_number.py
@@ -1,2 +1,7 @@
 # Prog04: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the lowest number
 
+num_list = []
+while True:
+    num = float(input("Enter a number: "))
+    num_list.append(num)
+    
