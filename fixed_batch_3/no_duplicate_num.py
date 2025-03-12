a3da71de1104cab42c57512b23471e869f525482
Adrian Kurt <dilanmarinas@gmail.com>
2025-03-12 16:28:47 +0800

    added condition to check if the number is in list

--- fixed_batch_3/no_duplicate_num.py
+++ fixed_batch_3/no_duplicate_num.py
@@ -1,5 +1,11 @@
-# Prog01: Create a program that ask user to input 10 numbers. Display all numbers that don't have duplicate.
+# Prog02: Create a program that ask user to input 10 numbers. Display all numbers. For numbers with duplicate, display only the first entry.
 
 num_list = []
 for i in range(10):
     num = float(input(f"Enter number {i + 1}: "))
+    if num in num_list:
+        pass
+    else:
+        num_list.append(num)
+
+print(num_list)
