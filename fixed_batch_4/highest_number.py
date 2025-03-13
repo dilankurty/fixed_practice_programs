339eb252c7eea0559772b3743e57a92f3764549d
Adrian Kurt <dilanmarinas@gmail.com>
2025-03-13 18:15:51 +0800

    fix spacing

--- fixed_batch_4/highest_number.py
+++ fixed_batch_4/highest_number.py
@@ -1,6 +1,7 @@
 # Prog03: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the highest number.
 
 num_list = []
+
 while True:
     try:
         num = float(input("Enter a number: "))
